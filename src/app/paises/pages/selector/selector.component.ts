40b6fe38f8c1fa89bbeceb8585ea79d6c6947f06
alejahndro02 <alejahndro02@gmail.com>
2022-03-12 13:05:51 -0600

    se creo el componente selector

---
+++ src/app/paises/pages/selector/selector.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: []
+})
+export class SelectorComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
