5fda76f3c6f260036f106478abe0cfd36d47172e
alejahndro02 <alejahndro02@gmail.com>
2022-03-13 08:43:40 -0600

    Se declararon las propieddades privadas _urlDB y _regiones, tambien se declaro un getter , se inyecto el sevicio de httpCient y se declaro el metodo getPaisesPorRegion

--- src/app/paises/service/paises-service.service.ts
+++ src/app/paises/service/paises-service.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+import { Injectable  } from '@angular/core';
+import { Observable  } from 'rxjs';
+import { PaisesSmall } from '../interface/paises';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesServiceService {
+  private _urlDb : string= `https://restcountries.com/v3.1`
+  private _regiones:string[] = [
+    'Africa',
+    'Europe',
+    'Asia',
+    'Americas',
+    'Oceania'
+  ]
+  /* Se crea un getter que retorna el arreglo 
+  haciendo una destructuracion para evitar modificar el arreglo  original*/
+  get regiones():string[]{
+    return [...this._regiones]
+  }
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPaisesPorRegion(region:string):Observable<PaisesSmall[]>{
+    const url = `${this._urlDb}/region/${region}?fields=cca3,name`
+    return this.http.get<PaisesSmall[]>( url)
+  }
 }
