c45eeb90155f95950ba36caa09c47cc367b92852
alejahndro02 <alejahndro02@gmail.com>
2022-03-15 00:48:38 -0600

    Se declaro lapropiedad de fronera tambien se utilizaron los operadores tap y switchMap para mutar la informacin de los subscribe coreespondiente

--- src/app/paises/pages/selector/selector.component.ts
+++ src/app/paises/pages/selector/selector.component.ts
@@ -23,9 +23,9 @@ export class SelectorComponent implements OnInit {
     frontera: ['', Validators.required]
   })
 // llenar los selectores
-  regiones:string[] = []
-  paises:PaisesSmall[]=[]
-  coodigoPais:Pais[]=[]
+  regiones    : string       []  = []
+  paises      : PaisesSmall  []  = []
+  fronteras   : string       []  = []
 
   constructor(  private fb: FormBuilder, 
                 private paisesService: PaisesServiceService ) { }
@@ -35,35 +35,47 @@ export class SelectorComponent implements OnInit {
 
     // Cuando cambie la reion 
     // Este codio es rempalzado con los operadores rxjs
-    // this.miformulario.get('region')?.valueChanges
-    //     .subscribe(region => {
-    //       console.log('esta',region);
-    //       this.paisesService.getPaisesPorRegion(region)
-    //           .subscribe(paises => {
-    //             console.log(paises);
-    //             this.paises = paises
-    //           })
-    //     }
-    //   )
+    /*this.miformulario.get('region')?.valueChanges
+        .subscribe(region => {
+          console.log('esta',region);
+          this.paisesService.getPaisesPorRegion(region)
+              .subscribe(paises => {
+                console.log(paises);
+                this.paises = paises
+              })
+        }
+      )*/
 
     this.miformulario.get('region')?.valueChanges
         .pipe(
-          //Con (_) es una nomenctura para indicar que no me interesa lo que viene por ese valor  
+          //Con (_) es una nomenctura para indicar que no me interesa lo que viene por ese valor  es lo mismo que dejar unicamente ()
           tap( (_) => {
             this.miformulario.get('pais')?.reset('');
             }),
           switchMap(region => this.paisesService.getPaisesPorRegion(region) )
         )
         .subscribe(paises => {
-          console.log(paises);
           this.paises = paises
         })
 
     // Cuando selecciona el pais imrpimra en sonsola el codio de su pais
     this.miformulario.get('pais')?.valueChanges
-    .subscribe(codigoPais =>{
-      console.log(codigoPais);
-    })
+      .pipe(
+        tap( (_)=> {
+          this.miformulario.get('frontera')?.reset('')
+        }),
+        switchMap(codigo => this.paisesService.getPaisPorCodigo(codigo))//esto regresa un pais 
+      )
+      .subscribe(pais => {
+        // Url V3
+        console.log('das',pais[0]?.borders);
+        this.fronteras= pais[0]?.borders || [];
+
+        // Url V2
+        /*console.log(pais?.borders);
+        this.fronteras= pais?.borders || [];*/
+      }
+    )
   }
 
   guardar(){
