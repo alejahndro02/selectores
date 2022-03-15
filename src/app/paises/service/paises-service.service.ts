581897eb5473756abe733c3e3411dfa28c52219a
alejahndro02 <alejahndro02@gmail.com>
2022-03-15 00:46:55 -0600

    Se delcaro una url con la V2 de la api

--- src/app/paises/service/paises-service.service.ts
+++ src/app/paises/service/paises-service.service.ts
@@ -8,6 +8,7 @@ import { Pais, PaisesSmall } from '../interface/paises';
 })
 export class PaisesServiceService {
   private _urlDb : string= `https://restcountries.com/v3.1`
+  private _urlDbv2 : string= `https://restcountries.com/v2`
   private _regiones:string[] = [
     'Africa',
     'Europe',
@@ -27,13 +28,18 @@ export class PaisesServiceService {
     const url = `${this._urlDb}/region/${region}?fields=cca3,name`
     return this.http.get<PaisesSmall[]>( url)
   }
-  getPaisPorCodigo(codigo:string):Observable<Pais| null> {
+  getPaisPorCodigo(codigo:string):Observable<Pais[]> {
     //Forma de Validacion si viene un string vacio en la respuesta 
     if(!codigo){
       // con of() se retorna un obsevable que tiene como resultado un null
-      return of (null)
+      return of ([])
     }
+    // UrlV2 dentro del retorno de tipo Pais[] se elimina el simbolo de array 
+    /*const url = `${this._urlDbv2}/alpha/${codigo}`;
+    return this.http.get<Pais>(url);*/
+    // Url V3
     const url = `${this._urlDb}/alpha/${codigo}`;
-    return this.http.get<Pais>(url);
+    return this.http.get<Pais[]>(url);
+
   }
 }
